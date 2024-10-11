5724a05c5578053c7d325c76f50fe7afdf4398fd
cnmdcurry <166487220+cnmdcurry@users.noreply.github.com>
2024-10-11 10:22:20 +0000

    10/11

--- test1.pl
+++ test1.pl
@@ -1,17 +1,30 @@
 #!/usr/bin/perl
 use warnings;
-
-$var='这是一个使用
-
-多行字符串文本
-
-的例子';
-
-print "$var \n";
-
+# 哈希
 =pod
-幹你娘
-操
-操你媽
-照你媽
-=cut    
+@age = (25, 30, 40);
+@name = ("John", "Paul", "Lisa");
+print " age = $age[0]\n";
+print " age = $age[1]\n";
+print " age = $age[2]\n";
+print " name = $name[0]\n";
+print " name = $name[1]\n"; 
+print " name = $name[2]\n";
+
+%data = ('John Paul', 45, 'Lisa', 30, 'Kumar', 40);
+print "John Paul's age = $data{'John Paul'}\n";
+print "Lisa's age = $data{'Lisa'}\n";
+print "Kumar's age = $data{'Kumar'}\n";
+=cut
+
+
+
+
+ 
+#!/usr/bin/perl
+ 
+@nums = (1..20);
+print "替换前 - @nums\n";
+ 
+splice(@nums, 5, 5, 21..25); 
+print "替换后 - @nums\n";
